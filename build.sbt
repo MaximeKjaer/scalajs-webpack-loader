acd2b7482a03d4941177afee4ba3fcf44c8e6453
Maxime Kjaer <maxime.kjaer@gmail.com>
2020-02-23 14:36:02 +0100

    Add external Scala typings for JS dependencies

--- build.sbt
+++ build.sbt
@@ -3,4 +3,10 @@ enablePlugins(ScalaJSPlugin)
 name := "scalajs-webpack-loader"
 scalaVersion := "2.13.1"
 
+resolvers += Resolver.bintrayRepo("oyvindberg", "ScalablyTyped")
+libraryDependencies ++= Seq(
+  "org.scalablytyped" %%% "loader-utils" % "1.1-dt-20180306Z-20260d",
+  "org.scalablytyped" %%% "webpack" % "4.41-dt-20200209Z-7b3b5d"
+)
+
 scalaJSLinkerConfig ~= { _.withModuleKind(ModuleKind.CommonJSModule) }
--- src/main/scala/io/kjaer/scalajs/webpack/Loader.scala
+++ src/main/scala/io/kjaer/scalajs/webpack/Loader.scala
@@ -1,27 +1,21 @@
 package io.kjaer.scalajs.webpack
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation.JSImport
-import scala.scalajs.js.annotation.JSExport
-import scala.scalajs.js.annotation.JSExportTopLevel
-import scala.scalajs.js.annotation.JSExportStatic
+import scala.scalajs.js.annotation._
+import typings.loaderUtils.mod.getOptions
+import typings.webpack.mod.loader.LoaderContext
 
 @js.native
 trait Options extends js.Object {
   val name: String
 }
 
-@js.native
-@JSImport("loader-utils", JSImport.Namespace)
-object LoaderUtils extends js.Object {
-  def getOptions(self: Any): Options = js.native
-}
-
 object Loader {
   @JSExportTopLevel("loader")
-  val loader: js.ThisFunction1[Any, String, String] = (self: Any, source: String) => {
-    val options = LoaderUtils.getOptions(self)
-    val replaced = source.replaceAll("""\[name\]""", options.name)
-    s"export default ${js.JSON.stringify(replaced)};"
-  }
+  val loader: js.ThisFunction1[LoaderContext, String, String] =
+    (self: LoaderContext, source: String) => {
+      val options = getOptions(self).asInstanceOf[Options]
+      val replaced = source.replaceAll("""\[name\]""", options.name)
+      s"export default ${js.JSON.stringify(replaced)};"
+    }
 }
